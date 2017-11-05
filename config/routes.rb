33765a9ba87729f186610497d572d2d8af99f82e
newswim <danminshew@gmail.com>
2017-11-05 17:30:27 -0600

    Extract title provider to application layout, write controller tests for static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'static_pages/home'
-
   get 'static_pages/help'
-
-    root 'application#hello'
+  get 'static_pages/about'
+  get 'static_pages/contact'
 end
