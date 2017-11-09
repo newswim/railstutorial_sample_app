844c49d3ff6be8a885fcae9f5bcb2eb5b25747ec
newswim <danminshew@gmail.com>
2017-11-08 19:48:17 -0600

    Generate a new Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'#, as: 'helf'
   get '/about', to: 'static_pages#about'
